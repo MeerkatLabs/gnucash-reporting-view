9c2267bb068b896c1c393ae1ae0bbeb2b14c93e0
Robert Robinson <rerobins@gmail.com>
2015-10-05 16:19:51 -0600

    Add category for expenses

--- src/reports/category/_application.js
+++ src/reports/category/_application.js
@@ -3,4 +3,5 @@ angular.module('gnucash-reports-view.reports.category', ['gnucash-reports-view.r
                                                           'nvd3'])
     .config(['ReportsManagementProvider', function(provider) {
         provider.addTemplate('account_usage_categories', 'src/reports/category/category.html');
+        provider.addTemplate('expenses_categories', 'src/reports/category/category.html');
     }]);
