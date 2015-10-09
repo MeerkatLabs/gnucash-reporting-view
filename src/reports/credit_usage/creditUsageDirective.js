b81f660a5dc5bfca903c56b58187c88dc107f2a8
Robert Robinson <rerobins@gmail.com>
2015-10-09 11:04:07 -0600

    Remove piechart from credit usage

--- src/reports/credit_usage/creditUsageDirective.js
+++ src/reports/credit_usage/creditUsageDirective.js
@@ -1,52 +1,74 @@
 /**
  * Created by rerobins on 9/29/15.
  */
-var CreditUsageDirectiveGenerator = function(formatters) {
+var CreditUsageDirectiveGenerator = function($timeout, formatters) {
+
+    var createCreditUsageChart = function($scope) {
+        var data = $scope.reportData();
+
+        $scope.options = {
+            chart: {
+                type: 'multiBarHorizontalChart',
+                transitionDuration: 0,
+                tooltip: {
+                    valueFormatter: formatters.currency
+                },
+                tickFormat: formatters.currencyNoParts,
+                x: function(d){return d.label;},
+                y: function(d){return d.value;},
+                showControls: false,
+                showValues: true,
+                xAxis: {
+                    showMaxMin: false
+                },
+                stacked: true,
+                yAxis: {
+                    ticks: 15,
+                    axisLabel: 'USD',
+                    tickFormat: formatters.currencyNoParts
+                },
+                margin: {
+                    left: 75,
+                    right: 75
+                }
+
+            }
+        };
+
+        var label = formatters.currency(data.credit_limit);
+
+        $scope.data = [
+            {
+                key: "Used",
+                values: [
+                    {
+                        label: label,
+                        value: data.credit_amount
+                    }
+                ]
+            },
+            {
+                key: "Available",
+                values: [
+                    {
+                        label: label,
+                        value: data.credit_limit - data.credit_amount
+                    }
+                ]
+            }
+        ];
+    };
+
     return {
         scope: {
             reportData: '&'
         },
         templateUrl: 'src/reports/credit_usage/creditUsageDirective.html',
         link: function($scope) {
-            var data = $scope.reportData();
-
-            $scope.options = {
-                chart: {
-                    type: 'pieChart',
-                    height: 300,
-                    x: function(d){return d.key;},
-                    y: function(d){return d.y;},
-                    showLabels: true,
-                    transitionDuration: 0,
-                    labelThreshold: 0.01,
-                    legend: {
-                        margin: {
-                            top: 5,
-                            right: 35,
-                            bottom: 5,
-                            left: 0
-                        }
-                    },
-                    labelType: "value",
-                    donut: true,
-                    labelsOutside: true,
-                    valueFormat: formatters.currency
-                }
-            };
-
-            $scope.data = [
-                {
-                    key: "Available",
-                    y: data.credit_limit
-                },
-                {
-                    key: "Used",
-                    y: data.credit_amount
-                }
-            ];
+            $timeout(createCreditUsageChart, 0, true, $scope);
         }
     };
 };
 
 angular.module('gnucash-reports-view.reports.credit_usage')
-    .directive('creditUsage', ['formatters', CreditUsageDirectiveGenerator]);
+    .directive('creditUsage', ['$timeout', 'formatters', CreditUsageDirectiveGenerator]);
