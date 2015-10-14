ad966b891e6b0c6a9569c98c42ce2c8429f4b97d
Robert Robinson <rerobins@gmail.com>
2015-10-14 15:55:38 -0600

    Add support for expense_account reports.

--- src/reports/category/_application.js
+++ src/reports/category/_application.js
@@ -2,7 +2,9 @@
 angular.module('gnucash-reports-view.reports.category', ['gnucash-reports-view.reports.base',
                                                           'nvd3'])
     .config(['ReportsManagementProvider', function(provider) {
-        provider.addTemplate('account_usage_categories', 'src/reports/category/category.html');
-        provider.addTemplate('expenses_categories', 'src/reports/category/category.html');
-        provider.addTemplate('investment_allocation', 'src/reports/category/category.html');
+        var categoryTemplate = 'src/reports/category/category.html';
+        provider.addTemplate('account_usage_categories', categoryTemplate);
+        provider.addTemplate('expenses_categories', categoryTemplate);
+        provider.addTemplate('investment_allocation', categoryTemplate);
+        provider.addTemplate('expense_accounts', categoryTemplate);
     }]);
